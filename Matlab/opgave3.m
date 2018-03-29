742f7ee0d4b07ed5aa47aa6bf83acb35ec5673a2
Bo Kleynen <kleynenbo@gmail.com>
2018-03-29 22:51:54 +0200

    opgave 3

--- Matlab/opgave3.m
+++ Matlab/opgave3.m
@@ -4,15 +4,13 @@ uo = @(y) 2;
 uz = @(x) 2 + sin(pi*x/2);
 un = @(x) 1;
 
-U = PDE(N, f, uw, uo, uz, un);
+U = PDE(@f, N, uw, uo, uz, un);
 h = 1/(N+1);
 [X, Y] = meshgrid(h:h:1-h);
 surf(X, Y, U)
+contour(X,Y,U)
 
 function z = f(x, y)
-    if (0.4 <= x) && (x <= 0.6) && (0.4 <= y) && (y <= 0.6)
-        z = -100;
-    else
-        z = 0;
-    end
+    temp = (0.4 <= x) & (x <= 0.6) & (0.4 <= y) & (y <= 0.6);
+    z = temp .* (-100);
 end
