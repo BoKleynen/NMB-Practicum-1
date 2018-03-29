6d767304574fbc7b9a016c74828ff6039865f882
Wim Kunnen <wim.kunnen@student.kuleuven.be>
2018-03-29 22:22:03 +0200

    Solution for question 2

--- Matlab/opgave2.m
+++ Matlab/opgave2.m
@@ -4,9 +4,17 @@ uo = @(y) exp(1+y);
 uz = @(x) exp(x);
 un = @(x) exp(1+x);
 f = @(x,y) 2*exp(x+y);
-N=256;
-h = 1/(N+1);
-
-[X,Y] = meshgrid(h:h:1-h);
-U = u(X,Y);
-max(max(abs((PDE(f, N, uw, uo, uz, un) - U) ./ U)))
+% N=256;
+% h = 1/(N+1);
+% 
+% [X,Y] = meshgrid(h:h:1-h);
+% U = u(X,Y);
+% max(max(abs((PDE(f, N, uw, uo, uz, un) - U) ./ U)))
+Result = [];
+for n = 3:10
+    h = 1/((2.^n)+1);
+    [X,Y] = meshgrid(h:h:1-h);
+    U = u(X,Y);
+    Result = [Result; max(max(abs((PDE(f, 2.^n, uw, uo, uz, un) - U) ./ U)))];
+end
+Result
