1ac3e1810cc6a4d26f46f04e6a4dfa5481153116
Bo Kleynen <kleynenbo@gmail.com>
2018-03-29 20:46:56 +0200

    wip

--- Matlab/PDE.m
+++ Matlab/PDE.m
@@ -3,16 +3,15 @@ function [U] = PDE(f, N, u_w, u_o, u_z, u_n)
    
    [F, ~] = meshgrid(0:h:1);
    F = f(F)*h^2;
-   F(1,:) = F(1,:) - u_w(0:h:N+2);
-   F(N+2,:) = F(N+2,:) - u_o(0:h:N+2);
-   F(:,1) = F(:,1) - u_z(0:h:N+2)';
-   F(:,N+2) = F(:,N+2) - u_n(0:h:N+2)';
-   F
+   F(1,:) = F(1,:) - u_w(0:h:N);
+   F(N,:) = F(N,:) - u_o(0:h:N);
+   F(:,1) = F(:,1) - u_z(0:h:N)';
+   F(:,N) = F(:,N) - u_n(0:h:N)';
    dstF = dst(dst(F')');
    dstU = zeros(N,N);
    
-   for k = 2:N+1
-       for l = 2:N+1
+   for k = 1:N
+       for l = 1:N
            dstU(k,l) = dstF(k,l)/(2*cos(pi*k*h)+cos(pi*l*h)-2);
        end
    end
