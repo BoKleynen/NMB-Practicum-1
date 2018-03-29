1de3a1ec4b028d50e31ebdea47c537e0e455edf6
Bo Kleynen <kleynenbo@gmail.com>
2018-03-29 21:52:41 +0200

    wip

--- Matlab/opgave2.m
+++ Matlab/opgave2.m
@@ -3,10 +3,10 @@ uw = @(y) u(0,y);
 uo = @(y) u(1,y);
 uz = @(x) u(1,0);
 un = @(x) u(x,1);
-f = @(x,y) exp(x+y);
-N=8;
+f = @(x,y) 2*exp(x+y);
+N=1024;
 h = 1/(N+1);
 
 [X,Y] = meshgrid(h:h:1-h);
-F = f(X,Y);
-max(max((PDE(f, N, uw, uo, uz, un) - F) ./ F))
+U = u(X,Y);
+max(max(abs((PDE(f, N, uw, uo, uz, un) - U) ./ U)))
