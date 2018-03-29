356d9dcd63bf3c94e4d09d75671949c514d5a6eb
Wim Kunnen <wim.kunnen@student.kuleuven.be>
2018-03-29 22:05:13 +0200

    Opgave 2 fix

--- Matlab/opgave2.m
+++ Matlab/opgave2.m
@@ -1,10 +1,10 @@
 u = @(x,y) exp(x+y);
-uw = @(y) u(0,y);
-uo = @(y) u(1,y);
-uz = @(x) u(1,0);
-un = @(x) u(x,1);
+uw = @(y) exp(y);
+uo = @(y) exp(1+y);
+uz = @(x) exp(x);
+un = @(x) exp(1+x);
 f = @(x,y) 2*exp(x+y);
-N=1024;
+N=256;
 h = 1/(N+1);
 
 [X,Y] = meshgrid(h:h:1-h);
