6c4923691bfd9f802518f630fd24058c35a307d4
Bo Kleynen <kleynenbo@gmail.com>
2018-03-29 20:14:26 +0200

    PDE v1

---
+++ Matlab/PDE.m
@@ -0,0 +1,20 @@
+function [U] = PDE(f, N, u_w, u_o, u_z, u_n)
+   h = 1/(N+1);
+   
+   [F, ~] = meshgrid(0:h:N+1);
+   F = f(F)*h^2;
+   F(1,:) = F(1,:) - u_w(0:h:N+1);
+   F(N,:) = F(N,:) - u_o(0:h:N+1);
+   F(:,1) = F(:,1) - u_z(0:h:N+1)';
+   F(:,N) = F(:,N) - u_n(0:h:N+1)';
+   dstF = dst(dst(F')');
+   dstU = zeros(N,N);
+   
+   for k = 1:N
+       for l = 1:N
+           dstU(k,l) = dstF(k,l)/(2*cos(pi*k*h)+cos(pi*l*h)-2);
+       end
+   end
+   
+   U = idst(idst(dstU')');
+end
