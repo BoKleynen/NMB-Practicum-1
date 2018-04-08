9ccb720aa698085190dd0790d4abdbb200b8ec10
Bo Kleynen <kleynenbo@gmail.com>
2018-04-08 23:51:03 +0200

    fix first range mistake

--- Matlab/createM.m
+++ Matlab/createM.m
@@ -20,7 +20,7 @@ args:
         for k = 2:order+2
             BSplines(j-k) = (tArr(j+k+1) - x)/(tArr(j+k+1) - tArr(j+1));
             
-            for l = j:-1:j-k+1
+            for l = j-1:-1:j-k
                 i = j-l;
                 BSplines(i) = (x - tArr(i))/(tArr(i+k) - tArr(i))*BSplines(i) ...
                                 + (tArr(i+k+1) - x)/(tArr(i+k+1) - tArr(i+1))*BSplines(i+1);
