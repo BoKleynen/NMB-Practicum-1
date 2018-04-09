89aba83dd4017d57c7bbdd41a6699bed3c09831b
Bo Kleynen <kleynenbo@gmail.com>
2018-04-09 09:37:40 +0200

    fix bad namings

--- Matlab/kkb_spline.m
+++ Matlab/kkb_spline.m
@@ -1,10 +1,10 @@
-function z = kkb_spline(tArr,xArr,f,y,k)
+function z = kkb_spline(tArr,xArr,fArr,yArr,degree)
 %{
 args:
     t: array of knot points (length n+2k+1).
     x: array of x-values for which f(x) is known (length r).
-    f: array of function values (length r).
-    y: array of x-values for which an approximation has to be calculated
+    fArr: array of function values (length r).
+    yArr: array of x-values for which an approximation has to be calculated
        (length N).
     k: degree of the B-splines.
 
@@ -12,26 +12,26 @@ result:
     z: array of approximations of y (length N).
 %}
 
-    if length(f) ~= length(xArr)
-        error('f and x should have the same dimensions.');
+    if length(fArr) ~= length(xArr)
+        error('fArr and x should have the same dimensions.');
     end
-    r = length(f);
-    n = length(t) - 2*k - 1;
+    r = length(fArr);
+    n = length(t) - 2*degree - 1;
     
     indexArr = zeros(r);
     for i = 1:r
         indexArr(i) = binarySearch(xArr(i), t);
     end
     
-    M = zeros(r, n+order);
+    M = zeros(r, n+degree);
     for row = 1:r
-        j = order;
+        j = degree+1;
         x = xArr(row);
         
         BSplines = zeros(j);
         BSplines(j) = 1;
         
-        for k = 1:order
+        for k = 1:degree+1
             BSplines(j-k) = (tArr(j+k+1) - x)/(tArr(j+k+1) - tArr(j+1));
             
             for i = j-k+1:j-1
@@ -45,10 +45,10 @@ result:
         M(row,indexArr(row)-k:indexArr(row)) = BSplines;
     end
     
-    c = f\M;
+    c = fArr\M;
     
-    z = zeros(length(y));
-    for i = 1:length(y)
-        z(i) = deBoor(xArr, t, k, c, indexArr(i));
+    z = zeros(length(yArr));
+    for i = 1:length(yArr)
+        z(i) = deBoor(xArr, tArr, order, c, indexArr(i));
     end
 end
