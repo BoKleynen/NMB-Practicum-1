e7c21b3a4f5522554ad04b9db6454a9cfbf68e96
Bo Kleynen <kleynenbo@gmail.com>
2018-04-11 10:34:07 +0200

    add opgave 7

---
+++ Matlab/opgave7.m
@@ -0,0 +1,19 @@
+t0 = -1;
+tn = 1;
+k = 3;
+
+x = linspace(t0, tn, 20)';
+f = sin(20*x)./(100*x.^2+5);
+f_ruis = f + 0.04*randn(size(x));
+err = zeros(10, 3);
+
+for q = 2:10
+    n = 2^q;
+    t = knotPoints(t0, tn, k, (tn-t0)/n);
+    z = kkb_spline(t, x, f_ruis, x, k);
+    err(q, 1) = n;
+    err(q, 2) = norm(f - z);
+    err(q, 3) = norm(f_ruis - z);
+end
+
+plot(err(:,1), err(:,2), 'r', err(:,1), err(:,3), 'b');
