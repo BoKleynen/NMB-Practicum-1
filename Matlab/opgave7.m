248e66c9c180306c4194ad8ce6279abf9da21364
Wim Kunnen <wim.kunnen@student.kuleuven.be>
2018-04-12 20:27:46 +0200

    Add figures for opgave 7

--- Matlab/opgave7.m
+++ Matlab/opgave7.m
@@ -15,4 +15,23 @@ for n = 10:300
     err(n-9, 3) = norm(f_ruis - z);
 end
 
+fig = figure;
 plot(err(:,1), err(:,2), 'r', err(:,1), err(:,3), 'b');
+saveas(fig,'../Verslag/ErrorBSplines.eps','epsc')
+
+[minimumErrorF, minimumNF] = min(err(:,2));
+[minimumErrorFRuis, minimumNFRuis] = min(err(:,3));
+
+t = knotPoints(t0, tn, k, minimumNF);
+z = kkbSpline(t, x, f, x, k);
+
+fig2 = figure;
+plot(x,z,'r',x,f,'b')
+saveas(fig2,'../Verslag/PlotSplinesNoNoise.eps','epsc')
+
+t = knotPoints(t0, tn, k, minimumNFRuis);
+z_ruis = kkbSpline(t, x, f_ruis, x, k);
+
+fig3 = figure;
+plot(x,f_ruis,'r',x,z_ruis,'b')
+saveas(fig3,'../Verslag/PlotSplinesNoise.eps','epsc')
